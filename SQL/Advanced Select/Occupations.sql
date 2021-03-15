09650b2412934bb3cfdf7b0133220d0e8639add0
Aditya Choudhary <78849471+adityaltts@users.noreply.github.com>
2021-03-15 17:06:36 +0530

    SQL

--- SQL/Advanced Select/Occupations.sql
+++ SQL/Advanced Select/Occupations.sql
@@ -1,18 +1,18 @@
-set @d=0, @p=0, @s=0, @a=0;
+SET @d=0, @p=0, @s=0, @a=0;
 
-select min(Doctor), min(Professor), min(Singer), min(Actor)
-from(
-  select case 
+SELECT min(Doctor), min(Professor), min(Singer), min(Actor)
+FROM(
+  SELECT case 
             when Occupation='Doctor' then (@d:=@d+1)
             when Occupation='Professor' then (@p:=@p+1)
             when Occupation='Singer' then (@s:=@s+1)
             when Occupation='Actor' then (@a:=@a+1) 
-            end as Row,
-        case when Occupation='Doctor' then Name end as Doctor,
-        case when Occupation='Professor' then Name end as Professor,
-        case when Occupation='Singer' then Name end as Singer,
-        case when Occupation='Actor' then Name end as Actor
-  from OCCUPATIONS
-  order by Name
-) as temp
-group by Row;
+            end AS Row,
+        case when Occupation='Doctor' then Name end AS Doctor,
+        case when Occupation='Professor' then Name end AS Professor,
+        case when Occupation='Singer' then Name end AS Singer,
+        case when Occupation='Actor' then Name end AS Actor
+  FROM OCCUPATIONS
+  ORDER BY Name
+) AS temp
+GROUP BY Row;
